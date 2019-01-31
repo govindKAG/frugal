dfb7f8e4b806fee21b21a55a01e6e4a3c0fd19af
govind somalal <govindsomalal7@gmail.com>
2019-01-31 11:36:10 +0530

    initial implemnentation of the item class to encapsulate items that will be graphed

---
+++ item.py
@@ -0,0 +1,28 @@
+class Item(object):
+    def __init__(self):
+        self.price = None
+        self.years = None
+        self.usage = None
+        self.label = None
+
+    def called(self, label):
+        self.label = label
+        return self
+
+    def whichcosts(self, price):
+        self.price = price
+        return self
+
+    def whichlasts(self, years):
+        self.years = years
+        return self
+
+    def used(self, usage):
+        self.usage = usage
+        return self
+
+    def __str__(self):
+        return f"""{self.label}
+        lasted {self.years}
+        use per year {self.usage}
+        cost {self.price}"""
